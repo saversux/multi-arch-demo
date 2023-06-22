96fc87cec77db5c8c08d0833942fc6a977bb7a03
Julien Bernhart <julienbernhart@gmail.com>
2023-06-22 11:36:30 +0200

    add k8s deployment for demo, move endpoint to root path

--- src/main/java/de/bernhart/GreetingResource.java
+++ src/main/java/de/bernhart/GreetingResource.java
@@ -5,12 +5,12 @@ import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
-@Path("/hello")
+@Path("/")
 public class GreetingResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
     public String hello() {
-        return "Hello from RESTEasy Reactive";
+        return "Hello, I am running on: " + System.getProperty("os.arch");
     }
 }
